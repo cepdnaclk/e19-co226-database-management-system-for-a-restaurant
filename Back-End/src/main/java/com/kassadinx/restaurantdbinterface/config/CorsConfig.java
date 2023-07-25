18af21efa97a48c5bf9054e0d91edb294b81ffd8
G5andeepD <e19063@eng.pdn.ac.lk>
2023-07-25 07:15:15 +0530

    Cors config

--- Back-End/src/main/java/com/kassadinx/restaurantdbinterface/config/CorsConfig.java
+++ Back-End/src/main/java/com/kassadinx/restaurantdbinterface/config/CorsConfig.java
@@ -1,2 +1,18 @@
-package com.kassadinx.restaurantdbinterface.config;public class CorsConfig {
+package com.kassadinx.restaurantdbinterface.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedOrigins("*");
+
+    }
 }
